b313aa3dec954eb54571c01ed821076ecf49859f
Tobias Gertz <tobialex.gertz@gmail.com>
2025-07-14 12:32:18 +0200

    Implemented image upload

--- src/routes/memory.py
+++ src/routes/memory.py
@@ -8,8 +8,10 @@ def upload():
     if request.method == "GET":
         return render_template('memory_upload.html', title="Homepage")
     if request.method == "POST":
+        image_file = request.files.get("image")
 
         MemoryManagement.upload_memory(description=request.form["description"],
-                                       date=request.form["date"])
+                                       date=request.form["date"],
+                                       image_file=image_file)
 
     return render_template('memory_upload.html', title="Homepage")
